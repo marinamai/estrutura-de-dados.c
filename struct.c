5a6d356b65060c5e3f49b23703bc7325a4a74d2c
marinamai <142596833+marinamai@users.noreply.github.com>
2024-04-10 10:43:39 -0300

    Update struct.c

--- struct.c
+++ struct.c
@@ -1,25 +1,26 @@
-//
+
 // main.c
 // Struct_Practice
-//
-// Created by Victor Zerefos on 10/04/24.
-//
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
 //Estrutura Livro
 typedef struct _livro{
 char titulo[100];
 unsigned int numeroPaginas;
 float preco;
 } Livro;
+
 //Estrutura Aluno
 typedef struct _aluno {
 char nome[100];
 unsigned int idade;
 Livro *livro_fav;
 } Aluno;
+
 // cria livro
 Livro *create_livro(const char *titulo, unsigned int numeroPaginas, float preco)
 {
@@ -29,6 +30,7 @@ livro->numeroPaginas = numeroPaginas;
 livro->preco = preco;
 return livro ;
 }
+
 // imprime livro
 void display_livro(const Livro *livro){
 printf("Titulo: %s\n", livro->titulo);
@@ -36,6 +38,7 @@ printf("Paginas: %d\n", livro->numeroPaginas);
 printf("Preço: R$%.2f\n", livro->preco);
 puts("");
 }
+
 // faz a cópia de um livro
 Livro *copy_livro(const Livro *livro){
 return create_livro(livro->titulo, livro->numeroPaginas, livro->preco);
@@ -43,12 +46,14 @@ return create_livro(livro->titulo, livro->numeroPaginas, livro->preco);
 void update_livro(Livro *livro_ref, float novoPreco){
 livro_ref->preco = novoPreco;
 }
+
 //desaloca livro da memoria
 void deallocate_livro(Livro **livro_ref){
 Livro *livro = *livro_ref;
 free(livro);
 *livro_ref = NULL;
 }
+
 // cria aluno
 Aluno *create_aluno(const char *nome, unsigned int idade, const Livro
 *livro_fav){
@@ -58,6 +63,7 @@ aluno->idade = idade;
 aluno->livro_fav = copy_livro(livro_fav);
 return aluno;
 }
+
 // imprimi aluno
 void display_aluno(const Aluno *aluno){
 printf("Nome: %s\n", aluno->nome);
@@ -67,6 +73,7 @@ printf("Livro Favorito do %s\n", aluno->nome);
 puts("-------------------------");
 display_livro(aluno->livro_fav);
 }
+
 // desaloca aluno
 void deallocate_aluno(Aluno **aluno_ref){
 Aluno *aluno = *aluno_ref;
@@ -74,6 +81,7 @@ deallocate_livro(&aluno->livro_fav);
 free(aluno);
 *aluno_ref = NULL;
 }
+
 // Bloco de execução MAIN
 int main(int argc, const char * argv[]) {
 Livro *lord_of_the_ring = create_livro("Lord Of The Ring", 280, 129);
