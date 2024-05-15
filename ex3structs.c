f86d8f7c6f484e8f7a88777c2a77e008069e6fbe
marinamai <142596833+marinamai@users.noreply.github.com>
2024-05-14 23:44:04 -0300

    Update ex3structs.c

--- ex3structs.c
+++ ex3structs.c
@@ -9,6 +9,46 @@
 
 typedef struct _produto{
     char nome[50]; 
-    int QuantidadeEstoqye;
+    int estoque;
     float preço;
 }Produto;
+
+Produto *createProduto(char *nome, int estoque, float preço){
+    Produto *produto = calloc (1, sizeof (Produto));
+    strcpy (produto-> nome, nome);
+    produto ->estoque = estoque;
+    produto -> preço = preço;
+return produto;
+}
+
+void deslocaProduto(Produto *produto){
+    free (produto);
+}
+
+void aumentarEstoque (Produto *produto, int quantidade){
+    produto -> estoque += quantidade;
+}
+void infoProduto(Produto *produto){
+        printf("Nome: %s\n", produto->nome);
+        printf("Estoque: %d\n", produto->estoque);
+        printf("Preço: %.2f\n", produto->preço);
+        puts("#####################");
+    }
+int main(){
+    Produto *banana = createProduto ("Banana", 10, 6.99);
+    Produto *uva = createProduto ("Uva", 20, 9.99);
+
+    infoProduto(banana);
+    infoProduto(uva);
+
+    aumentarEstoque(banana, 3);
+    aumentarEstoque(uva, 2);
+
+    infoProduto(banana);
+    infoProduto(uva);
+
+    desalocarProduto(banana);
+    desalocarProduto(uva);
+
+    return 0;
+}
